5e2a0727f93d37d6f3045129c3537980a2d78d0e
Matias Raimondi <matiraimondi1@gmail.com>
2025-02-12 20:20:49 -0300

    Index.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import User from "./user";
 
+async function main() {
+    try {
+        const users = await User.obtainAll();
+        console.log(users);
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+}
+
+main();
