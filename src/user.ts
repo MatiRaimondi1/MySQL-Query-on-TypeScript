82138187c8f5d36617ce4a8468425771488e174a
Matias Raimondi <matiraimondi1@gmail.com>
2025-02-12 20:18:29 -0300

    User class.

---
+++ src/user.ts
@@ -0,0 +1,13 @@
+import Database from "./database";
+import { RowDataPacket } from 'mysql2/promise';
+
+export default class User {
+    constructor(id: number, name: string) {}
+
+    static async obtainAll(): Promise<User[]> {
+        const db = Database.getInstance();
+        await db.connect();
+        const rows = await db.query<RowDataPacket[]>('SELECT * FROM user');
+        return rows.map(row => new User(row.id, row.name));
+    }
+}
